1049f66afca3155096966750bb5c7a3ead404d2c
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-01-24 16:29:59 +0200

    modified:   app.js deleted:    message.txt modified:   package-lock.json modified:   package.json deleted:    routes.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const http = require('http');
 
-const { requestHandler, someText } = require('./routes');
+const express = require('express');
 
-console.log(someText);
+const app = express();
 
-const server = http.createServer(requestHandler);
+app.use((req, res, next) => {
+  console.log('First middleware function');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Second middleware function');
+  res.send('<h1>Hello from Express!</h1>')
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
