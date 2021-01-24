2e8093c89391f1cc7b54e5f9eaee775a6d1dc411
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-01-24 18:27:31 +0200

    modified:   app.js new file:   routes/admin.js new file:   routes/shop.js

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware function');
-  next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((req, res, next) => {
-  console.log('Second middleware function');
-  res.send('<h1>Hello from Express!</h1>')
-});
+app.use(bodyParser.urlencoded({ extended: false }))
 
-const server = http.createServer(app);
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-server.listen(3000);
+app.listen(3000);
