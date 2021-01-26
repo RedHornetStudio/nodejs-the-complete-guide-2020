628aaaddc710389cd90c8436179d07f8d7a00c16
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-01-26 12:57:40 +0200

    modified:   app.js modified:   package.json new file:   public/css/main.css new file:   public/css/product.css modified:   routes/admin.js modified:   routes/shop.js new file:   util/path.js new file:   views/404.html new file:   views/add-product.html new file:   views/shop.html

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,18 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
+// /admin/add-product => GET (/admin route is pointed in app.js file)
 router.get('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"</input><button type="submit">Add Product</button></form>');
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
+// /admin/add-product => POST (/admin route is pointed in app.js file)
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
