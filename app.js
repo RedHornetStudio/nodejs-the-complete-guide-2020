6f529c14ee8e4714b463f0284d1a76cc6677a5ea
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-01-31 13:34:03 +0200

    templating engine ejs code finished modified:   app.js modified:   routes/admin.js modified:   routes/shop.js new file:   views/404.ejs deleted:    views/404.hbs deleted:    views/404.html deleted:    views/404.pug new file:   views/add-product.ejs deleted:    views/add-product.hbs deleted:    views/add-product.html deleted:    views/add-product.pug new file:   views/includes/end.ejs new file:   views/includes/head.ejs new file:   views/includes/navigation.ejs deleted:    views/layouts/main-layout.hbs deleted:    views/layouts/main-layout.pug new file:   views/shop.ejs deleted:    views/shop.hbs deleted:    views/shop.html deleted:    views/shop.pug

--- app.js
+++ app.js
@@ -2,21 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  }));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const port = 3000;
@@ -28,7 +20,10 @@ app.use('/admin', adminData.router);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', {
+    pageTitle: 'Page Not Found',
+    path: '/404'
+  });
 });
 
 app.listen(port, () => {
